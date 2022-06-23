db90e823cdbca11c21a5dc478cc59624a2ab7545
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-06-23 14:50:09 +0530

    object cloning

--- JavaScript/objects.js
+++ JavaScript/objects.js
@@ -34,18 +34,50 @@ for (let i = 0; i < Object.keys(captainAmerica).length; i++) {
 }
 
 //prompt
-let fruitname = prompt("Enter Any Fruit Name: ");
-let bag = {
-  [fruitname]: 10,
-};
+// let fruitname = prompt("Enter Any Fruit Name: ");
+// let bag = {
+//   [fruitname]: 10,
+// };
 /*
 dot natation can't evalue fruitname so it will look for fruitname key in bag object but here fruit name can be anything according to input
 */
-alert(bag.fruitname);
+// alert(bag.fruitname);
 
-alert(bag.banana); //it will show 10 only if banana will be entered
+// alert(bag.banana); //it will show 10 only if banana will be entered
 
 /*
 so we can use bracket notation for evaluating this and can get output for any input
 */
-alert(bag[fruitname]); //now it will for any input
+// alert(bag[fruitname]); //now it will for any input
+
+// Object references and copying
+
+let user = {
+  name: "Ujjwal",
+};
+
+let admin = user;
+admin.name = "Ankish";
+console.log(admin.name);
+
+//Cloning and merging, Object.assign
+let users = {
+  name: "John",
+  age: 30,
+};
+
+let clone = {}; // the new empty object
+
+for (key in users) {
+  clone[key] = users[key];
+}
+
+for (key in clone) {
+  console.log("Cloned Object: ", clone[key]);
+}
+
+//We can also use the method Object.assign.
+Object.assign(clone, users);
+for (key in clone) {
+  console.log("Copied Using Object Assign Method: " + clone[key]);
+}
