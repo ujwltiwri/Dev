13c390d699b1d07ccd4af26fa09836323868ec90
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-06-30 13:49:14 +0530

    deb

---
+++ JavaScript/hoisting.js
@@ -0,0 +1,86 @@
+// {
+//   var a = 10;
+//   let b = 100;
+//   const c = 1000;
+
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+
+// function fun() {
+//   console.log("Ujjwal");
+// }
+
+// fun();
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// console.log(a);
+// var ans = fun();
+// console.log(ans);
+// var a = 10
+// function fun(){
+//   console.log("FJP-9 Rocks");
+// }
+// console.log(greet);
+// var greet = function (){
+//   console.log("hello");
+// }
+// console.log(greet);
+// console.log(a);
+
+// greet();
+// console.log(greet);
+// var greet = function () {
+//   console.log("Heloo");
+// };
+// // greet();
+// console.log(greet);
+
+// {
+//   var a = 10;
+//   let b = 100;
+//   const c = 1000;
+
+//   console.log(a); //10
+//   console.log(b); //100
+//   console.log(c); //1000
+// }
+
+// console.log(a); //10
+// console.log(b); //not defined -> reference error
+// console.log(c); //not defined -> reference error
+
+// function fun() {
+//   if (true) {
+//     let a = 10;
+//     var b = 20;
+//   }
+//   console.log(b); // 20
+//   console.log(a); // not defined
+// }
+
+// fun();
+
+function news() {
+  console.log("Hello This is a news portal");
+}
+
+//if i print this func then it will console log the whole func definition instead of printing "Hello This is a news portal"
+console.log(news);
+//so to overcome i will simply call this function
+news();
+
+function gd(num) {
+  var res = num * num;
+  return res;
+}
+
+//print the function then it will show func defination and not real value
+console.log(gd);
+
+// now we will have to catch the func in a var first
+let print = gd(5);
+console.log(gd);
--- JavaScript/objects.js
+++ JavaScript/objects.js
@@ -1,83 +1,94 @@
-let captainAmerica = {
-  firstName: "Steve",
-  lastName: "Rogers",
-  friends: ["Bucky", "Tony Stark", "Bruce Banner", "Thor"],
-  Age: 122,
-  isAvenger: true,
+// let captainAmerica = {
+//   firstName: "Steve",
+//   lastName: "Rogers",
+//   friends: ["Bucky", "Tony Stark", "Bruce Banner", "Thor"],
+//   Age: 122,
+//   isAvenger: true,
 
-  address: {
-    State: "Manhattan",
-    City: "New York",
-    Country: "United States of America",
-  },
+//   address: {
+//     State: "Manhattan",
+//     City: "New York",
+//     Country: "United States of America",
+//   },
 
-  sayHi: function () {
-    console.log(`Hello My Name is ${captainAmerica.firstName}`);
-  },
-};
+//   sayHi: function () {
+//     console.log(`Hello My Name is ${captainAmerica.firstName}`);
+//   },
+// };
 
-captainAmerica.sayHi(); //called function from an onbject
+// captainAmerica.sayHi(); //called function from an onbject
 
-//access key using square bracket notation
-let square_bracket = captainAmerica["Age"];
-console.log("Square Bracket: ", square_bracket);
+// //access key using square bracket notation
+// let square_bracket = captainAmerica["Age"];
+// console.log("Square Bracket: ", square_bracket);
 
-//loop over on object
-for (key in captainAmerica) {
-  //   console.log("This is Key: ", key);
-  console.log(captainAmerica[key]); //here we are using bracket notation for accessing key's value
-}
+// //loop over on object
+// for (key in captainAmerica) {
+//   //   console.log("This is Key: ", key);
+//   console.log(captainAmerica[key]); //here we are using bracket notation for accessing key's value
+// }
 
-//for loop method for object access
-for (let i = 0; i < Object.keys(captainAmerica).length; i++) {
-  console.log(captainAmerica);
-}
+// //for loop method for object access
+// for (let i = 0; i < Object.keys(captainAmerica).length; i++) {
+//   console.log(captainAmerica);
+// }
 
-//prompt
-let fruitname = prompt("Enter Any Fruit Name: ");
-let bag = {
-  [fruitname]: 10,
-};
-/*
-dot natation can't evalue fruitname so it will look for fruitname key in bag object but here fruit name can be anything according to input
-*/
-alert(bag.fruitname);
+// //prompt
+// let fruitname = prompt("Enter Any Fruit Name: ");
+// let bag = {
+//   [fruitname]: 10,
+// };
+// /*
+// dot natation can't evalue fruitname so it will look for fruitname key in bag object but here fruit name can be anything according to input
+// */
+// alert(bag.fruitname);
 
-alert(bag.banana); //it will show 10 only if banana will be entered
+// alert(bag.banana); //it will show 10 only if banana will be entered
 
-/*
-so we can use bracket notation for evaluating this and can get output for any input
-*/
-alert(bag[fruitname]); //now it will for any input
+// /*
+// so we can use bracket notation for evaluating this and can get output for any input
+// */
+// alert(bag[fruitname]); //now it will for any input
 
 // Object references and copying
 
-let user = {
-  name: "Ujjwal",
-};
+// let user = {
+//   name: "Ujjwal",
+// };
 
-let admin = user;
-admin.name = "Ankish";
-console.log(admin.name);
+// let admin = user;
+// admin.name = "Ankish";
+// console.log(admin.name);
 
-//Cloning and merging, Object.assign
-let users = {
-  name: "John",
-  age: 30,
-};
+// //Cloning and merging, Object.assign
+// let users = {
+//   name: "John",
+//   age: 30,
+// };
 
-let clone = {}; // the new empty object
+// let clone = {}; // the new empty object
 
-for (key in users) {
-  clone[key] = users[key];
+// for (key in users) {
+//   clone[key] = users[key];
+// }
+
+// for (key in clone) {
+//   console.log("Cloned Object: ", clone[key]);
+// }
+
+// //We can also use the method Object.assign.It's Shorthand Property for cloning an object
+// Object.assign(clone, users);
+// for (key in clone) {
+//   console.log("Copied Using Object Assign Method: " + clone[key]);
+// }
+
+let user = { name: "John", age: 30 };
+let clone = {};
+
+for (let key in user) {
+  clone[key] = user[key];
 }
 
-for (key in clone) {
-  console.log("Cloned Object: ", clone[key]);
-}
-
-//We can also use the method Object.assign.
-Object.assign(clone, users);
-for (key in clone) {
-  console.log("Copied Using Object Assign Method: " + clone[key]);
-}
+console.log(clone);
+clone.name = "Pete";
+console.log(user.name);
