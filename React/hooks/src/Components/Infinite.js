09ed20d8176d51390e51feea73422d4a98356b37
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-10-12 10:58:43 +0530

    :zap: Hooks :zap:

--- React/hooks/src/Components/Infinite.js
+++ React/hooks/src/Components/Infinite.js
@@ -7,8 +7,8 @@ const Infinite = () => {
     console.log("useEffect is called");
     document.title = `Button is Clicked ${count} times`;
     let rnum = Math.random() * 100;
-    // setCount(rnum); //Here State Will Change Every Time cuz of rnum being updated each time & As setCount has Primitive Value & as there is Change every time in this primitive value so State Changes each time -> Then Render Method is Called -> Then Again as the value of rnum changes, State Changes Again -> Then Again Render Method is Called & Then State Changes -> and it goes into infinite loop
-    // setCount(13); //Here State Won't Change Because setCount has Primitive Value & as there is no Chnage in this primitive value so State Won't Change and UseEffect will be Called Only Once
+    // setCount(rnum); /* Here State Will Change Every Time cuz of rnum being updated each time & As setCount has Primitive Value & as there is Change every time in this primitive value so State Changes each time -> Then Render Method is Called -> Then Again as the value of rnum changes, State Changes Again -> Then Again Render Method is Called & Then State Changes -> and it goes into infinite loop */
+    // setCount(13); /* Here State Won't Change Because setCount has Primitive Value & as there is no Chnage in this primitive value so State Won't Change and UseEffect will be Called Only Once */
   });
 
   return (
