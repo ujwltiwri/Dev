12099a4773d0da8a05a8434b0c3cecbcb795d316
Ujjwal Tiwari <ujwltiwari494@gmail.com>
2022-12-04 00:09:31 +0530

    :zap: planRouter

---
+++ Backend/FoodApp/Routers/planRouter.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { protectRoute, isAuthorised } = require("../helper");
+const planRouter = express.Router();
+const {
+  getAllPlans,
+  getPlan,
+  createPlan,
+  updatePlan,
+  deletePlan,
+  top3Plans,
+} = require("../controller/planController");
+
+planRouter
+    .route('/all')
+    .get(getAllPlans);
+
+planRouter
+    .route('/top3')
+    .get(top3Plans);
+
+planRouter.use(protectRoute) //checks user logged in hai ya nhi 
+    .route('/single/:id')
+    .get(getPlan);
+
+planRouter.use(isAuthorised(['admin', 'restaurantowner'])) // logged in , but shoudl have proper role 
+planRouter
+    .route("/crud")
+    .post(createPlan)
+
+planRouter
+    .route('/crud/:id')
+    .patch(updatePlan)
+    .delete(deletePlan)
